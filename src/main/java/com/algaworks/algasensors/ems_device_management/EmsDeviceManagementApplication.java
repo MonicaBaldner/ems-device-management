7cbb07317b3a968cbb4825e554cc23584e92afa5
Mônica Baldner <baldnermonica@gmail.com>
2025-05-26 16:55:10 -0300

    New project files

---
+++ src/main/java/com/algaworks/algasensors/ems_device_management/EmsDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.ems_device_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsDeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsDeviceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algasensors/ems_device_management/EmsDeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.ems_device_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsDeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
