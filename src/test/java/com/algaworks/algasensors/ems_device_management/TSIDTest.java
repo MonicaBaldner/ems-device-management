e938c5a6d744b89be6d967ba6eb40aa7c7de7ab5
Mônica Baldner <baldnermonica@gmail.com>
2025-05-27 14:03:03 -0300

    atualização de device-management

---
+++ src/test/java/com/algaworks/algasensors/ems_device_management/TSIDTest.java
@@ -0,0 +1,37 @@
+package com.algaworks.algasensors.ems_device_management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class TSIDTest {
+
+    @Test
+    public void shouldGenerateTSID(){
+    /*  //  TSID tsid = TSID.fast();
+        TSID tsid = TSID.Factory.getTsid();
+        System.out.println(tsid);
+        System.out.println(tsid.toLong());
+
+     */
+
+        /*System.setProperty("tsid.node","2");
+        System.setProperty("tsid.node.count", "32");
+        TSID.Factory factory = TSID.Factory.builder().build();
+        TSID tsid = factory.generate();
+        System.out.println("O TSID gerado foi " + tsid);
+        System.out.println("O TSID gerado no formato Long " + tsid.toLong());
+        System.out.println("O instante no qual o TSID foi gerado " + tsid.getInstant());*/
+
+
+            TSID tsid = IdGenerator.generateTSID();
+            Assertions.assertThat(tsid.getInstant())
+                    .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+    }
+
+
+
+}
